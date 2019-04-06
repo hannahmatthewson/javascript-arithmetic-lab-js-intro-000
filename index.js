26b7b7a2ef53024594eee2a8567f96dfe9dd0365
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-06 10:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 
 function add(a,b) {
-   return a + b;
+  return a + b;
 
   var a = 2;
   var b = 3;
 }
 
 function subtract() {
+  return a - b;
 
+  var a = 10;
+  var b = 5;
 }
 
 
 function multiply() {
+  return a * b;
 
+  var a = 4;
+  var b = 5;
 }
 
 
 function divide() {
+  return a / b;
 
+  var a = 20;
+  var b = 2;
 }
